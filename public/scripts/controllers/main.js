47ad21aec85df6437dd8622984449ed417dfc7dc
Kevin Castor <kmcasto@users.noreply.github.com>
2014-12-02 16:34:17 -0600

    Update main.js

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -15,7 +15,6 @@ angular.module('metacriticApp')
 			$http.get('/get/platforms/5').
 				  success(function(data) {
 					// this callback will be called asynchronously
-					console.log('success');
 					console.log(data);
 					// when the response is available
 				  }).
@@ -24,7 +23,44 @@ angular.module('metacriticApp')
 					console.log('fail');
 					// or server returns response with an error status.
 				  });
+				  
+			 $scope.getSystems = function() {
+				console.log('sup');
+			}
+				  
+				  
+			/*
+				$scope.systems = [
+				{ name: 'ps4',    selected: true },
+				{ name: 'xboxone',   selected: false },
+				{ name: 'ps3',     selected: false },
+				{ name: 'xbox360', selected: false },
+				{ name: 'pc', selected: false },
+				{ name: 'wii-u', selected: false },
+				{ name: '3ds', selected: false },
+				{ name: 'vita', selected: false },
+				{ name: 'ios', selected: false },
+				];
+
+				// selected systems
+				$scope.selection = [];
+				var selections;
+
+				// helper method to get selected systems
+				$scope.selectedSystems = function selectedSystems() {
+				return filterFilter($scope.systems, { selected: true });
+				};
+
+
+				// watch systems for changes
+				$scope.$watch('systems|filter:{selected:true}', function (nv) {
+				selections = nv.map(function (system) {
+				console.log(system.name);
+				//buildGames(system.name);
+				return system.name;
+				});
+				}, true);
 			
-			
+		*/
 		
   }]);
